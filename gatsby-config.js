d3511763b08829df7099271e616bf81e3fee7d0c
autsada <autsada.t@gmail.com>
2019-11-30 12:07:40 +0700

    Renamed site title and changed logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mii Libra API`,
+    description: `Graphql API for interacting with the Libra Blockchain`,
+    author: `Autsada T`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -40,7 +40,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mii-api.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
